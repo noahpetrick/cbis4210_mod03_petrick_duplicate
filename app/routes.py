6077ce0ccde285d1d47c6b80a4452043c805edee
noahpetrick <noah.petrick@bobcats.gcsu.edu>
2024-09-30 00:10:16 -0400

    Final assignment for Module 03 completed.

---
+++ app/routes.py
@@ -0,0 +1,22 @@
+from flask import render_template
+from . import app
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+@app.route('/membership')
+def membership():
+    return render_template('membership.html')
+
+@app.route('/contact')
+def contact():
+    return render_template('contact.html')
+
+@app.route('/product')
+def product():
+    return render_template('product.html')
